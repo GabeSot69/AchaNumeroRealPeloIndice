7c04a67521c41b2f0f375b9057362770b27fb27a
GabeSot69 <105126836+GabeSot69@users.noreply.github.com>
2022-08-18 09:01:29 -0400

    Add files via upload

--- Ex61/src/Ex61.java
+++ Ex61/src/Ex61.java
@@ -1,10 +1,16 @@
+/*
+	Nome: Gabriel Sotero Mendonça
+	Objetivo: Faça um programa que solicite a digitação e armazene 20 números reais em um vetor. Depois o programa deve
+	ficar disponível para o usuário digitar o valor do índice para que seja exibido o número armazenado no índice
+	solicitado. Para encerrar o programa, o usuário deve informar um índice inválido
+ */
 import java.util.Scanner;
 
 public class Ex61 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		int numValores = 5;
-		double valores[] = new double[numValores];
+		int numValores = 20;
+		double[] valores = new double[numValores];
 		for (int i = 0; i < valores.length; i++) {
 			System.out.println("Digite um numero");
 			valores[i] = scanner.nextDouble();
@@ -19,8 +25,8 @@ public class Ex61 {
 				break;
 				
 			}
-			System.out.println("Valor no indice " + escolhaIndice + " " + valores[escolhaIndice]);
-			
+			System.out.println("Valor no indice " + escolhaIndice + ": " + valores[escolhaIndice]);
+			System.out.println();
 		}
 	}
 
